2a3be49e27c05eb5180b5b04be7ff7ba1bc1dc01
Kagheni <Kaghenimbale@gmail.com>
2023-04-17 12:28:21 +0200

    Add navigation bar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../images/planet.png';
+
+const Header = () => (
+  <div className="navbar">
+    <div className="logo">
+      <img src={logo} alt="logo" />
+      <p>Space Traveler&apos;s Hub</p>
+    </div>
+    <ul className="navlink">
+      <NavLink to="/">Rockets</NavLink>
+      <NavLink to="mission">Missions</NavLink>
+      <NavLink to="profile">My profile</NavLink>
+    </ul>
+  </div>
+);
+
+export default Header;
