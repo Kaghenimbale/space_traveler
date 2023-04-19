0906437ecaff0d7cab88c11c94860ec95f005be9
Kagheni <Kaghenimbale@gmail.com>
2023-04-19 08:57:28 +0200

    Add changes on the profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import './profile.css';
+import { useSelector } from 'react-redux';
 
-const Profile = () => <div>Profile</div>;
+const Profile = () => {
+  const { missionItems } = useSelector((state) => state.mission);
+  return (
+    <div className="content">
+      <div className="mission">
+        <h2>My Missions</h2>
+        <ul>
+          {missionItems.map((item) => <li key={item.mission_id}>{item.mission_name}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
